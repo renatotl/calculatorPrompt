44781dfb66f1203aa7f0d1c8bbf1c7650ee9fc89
Renato Tenório Lino <renatotl@outlook.com>
2024-04-28 22:09:00 -0300

    up

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const prompt = require(`prompt-sync`)();
 
-let valuer1  ;
+let valuer1;
+let valuer2;
+let simbol;
+
+if(simbol == '+'){
+result = valuer1 + valuer2;
+
+}else if(simbol == '-'){
+    result = valuer1 - valuer2;
+
+}else if(simbol == '*'){
+    result = valuer1 * valuer2;
+
+}else if(simbol == '/'){
+    result = valuer1 / valuer2;
+
+}else{
+let error = 'Opcao invalida'
+};

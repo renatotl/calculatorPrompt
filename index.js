60040403e34a107a7ce8199e75879cc51034340a
Renato Tenório Lino <renatotl@outlook.com>
2024-04-28 22:37:40 -0300

    up

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ let valuer1;
 let valuer2;
 let simbol;
 
-valuer1 = prompt();
+valuer1 = +prompt();
 
-simbol = prompt("");
+simbol = prompt('');
 
-valuer2 = prompt();
+valuer2 = +prompt();
 
 if(simbol == '+'){
 result = valuer1 + valuer2;
+console.log(result)
 
 }else if(simbol == '-'){
     result = valuer1 - valuer2;
@@ -24,4 +25,9 @@ result = valuer1 + valuer2;
 
 }else{
 let error = 'Opcao invalida'
+console.log(error)
 };
+
+
+
+// para executar esse arquivo no terminal :  node file.js 

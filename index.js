c4aa5ee590bd5028b6f768ff55871b3f630b018b
Renato Tenório Lino <renatotl@outlook.com>
2024-04-28 22:15:26 -0300

    up

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ let valuer1;
 let valuer2;
 let simbol;
 
-valuer1 = prompt("");
+valuer1 = prompt();
 
 simbol = prompt("");
 
-valuer2 = prompt("");
+valuer2 = prompt();
 
 if(simbol == '+'){
 result = valuer1 + valuer2;
